e16ac718b3f8305bb308e057d8535d46d4af3082
ktrips <kenichi_yoshida@hotmail.com>
2026-06-28 00:17:20 +0900

    a.1.3.22

---
+++ kmind/kmindTests/kmindTests.swift
@@ -0,0 +1,19 @@
+//
+//  kmindTests.swift
+//  kmindTests
+//
+//  Created by Kenichi Yoshida on 2026/06/27.
+//
+
+import Testing
+@testable import kmind
+
+struct kmindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
